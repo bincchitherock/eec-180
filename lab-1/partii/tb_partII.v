15f4bdffa7cdcb6cd46a4bbd1bfbb149849f5e1d
bini <seleneseobinhan@gmail.com>
2026-04-03 10:49:52 -0700

    Add files via upload
    testbench files in verilog including the dec_7seg_decoder.v for part II

---
+++ lab-1/partii/tb_partII.v
@@ -0,0 +1,31 @@
+module tb_partII;
+
+    reg [3:0] count;        // 4-bit counter to drive SW[3:0]
+
+    wire [7:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+    wire [1:0] KEY;
+    wire [9:0] LEDR;
+    wire [9:0] SW;
+
+    assign SW[3:0] = count; // connect counter to switch inputs
+
+    // instantiate the design under test
+    partII UUT (.HEX0(HEX0), .HEX1(HEX1), .HEX2(HEX2),
+                .HEX3(HEX3), .HEX4(HEX4), .HEX5(HEX5),
+                .KEY(KEY), .LEDR(LEDR), .SW(SW));
+
+    initial begin
+
+        $dumpfile("tb_partII.vcd");
+        $dumpvars(0, tb_partII);
+
+        count = 4'b0000;
+        repeat (16) begin   // 16 combos for 4 inputs
+            #100
+            $display("SW[3:0] = %b (%2d) | HEX1 = %b  HEX0 = %b",
+                      count, count, HEX1, HEX0);
+            count = count + 4'b0001;
+        end
+        $stop;
+    end
+endmodule
